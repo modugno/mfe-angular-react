fb2fee179f6da64c7210d0ab57f8c815364fa340
Guilherme Modugno <modugnoguilherme@gmail.com>
2022-01-22 18:53:39 -0300

    chore(angular-header): add angular header

--- packages/angular-app/src/app/app.module.ts
+++ packages/angular-app/src/app/app.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +16,13 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const { injector } = this
+    const ngCustomElementHeader = createCustomElement(HeaderComponent, { injector })
+    window.customElements.define('ng-header', ngCustomElementHeader)
+  }
+}
